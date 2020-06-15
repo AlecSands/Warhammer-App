809f052897c4f13bc696243c5f02405bf083a288
Alec Sands <sandsalecv@gmail.com>
2020-06-15 14:02:55 -0500

    Updates

---
+++ precache-manifest.182611f414f4b7fe0df01f97826fe628.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a240590fb20bce53408a3846a621a59",
+    "url": "/Warhammer-App/index.html"
+  },
+  {
+    "revision": "a07f33d9290bdf465d08",
+    "url": "/Warhammer-App/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e31d887ac90818d13e50",
+    "url": "/Warhammer-App/static/js/2.075a502a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Warhammer-App/static/js/2.075a502a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a07f33d9290bdf465d08",
+    "url": "/Warhammer-App/static/js/main.5614bdde.chunk.js"
+  },
+  {
+    "revision": "30d50baf2ae0bbbf0a80",
+    "url": "/Warhammer-App/static/js/runtime-main.3301c654.js"
+  }
+]);
