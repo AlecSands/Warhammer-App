1fb8ee7a86f35459a96b7685f40c3864b818ecea
Alec Sands <sandsalecv@gmail.com>
2020-06-16 14:52:31 -0500

    adding basic phase and action components

--- warhammer/src/App.js
+++ warhammer/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Phase from './Phase';
+import Action from './Action';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Warhammer Application</h1>
+      <Phase name="Phase 1 - Hero Phase"/>
+      <Action name="Spell Casts" />
     </div>
   );
 }
