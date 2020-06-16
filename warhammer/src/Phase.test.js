2c0a6bab06554bc61c7e2de9420e2be75374ab88
Alec Sands <sandsalecv@gmail.com>
2020-06-16 15:02:11 -0500

    changed test to reflect new desired functionality

--- warhammer/src/Phase.test.js
+++ warhammer/src/Phase.test.js
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react';
 import Phase from './Phase';
 
 test('renders phase name based on props', () => {
-  const { getByText } = render(<Phase name="Phase 1 - Hero Phase" />);
+  const { getByText } = render(<Phase name="Hero Phase" number="1" />);
   const linkElement = getByText(/Phase 1 - Hero Phase/i);
   expect(linkElement).toBeInTheDocument();
 });
