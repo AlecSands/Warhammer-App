cc1a2de5bb0ea04fd057db6b72d24184d5897f45
Alec Sands <sandsalecv@gmail.com>
2020-06-18 17:03:15 -0500

    adding edge case test for phase exceeding expected value

--- warhammer/src/Phase.test.js
+++ warhammer/src/Phase.test.js
@@ -2,14 +2,20 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import Phase from './Phase';
 
-// test('renders phase name based on props', () => {
-//   const { getByText } = render(<Phase name="Hero Phase" number="1" />);
-//   const linkElement = getByText(/Phase 1 - Hero Phase/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
-
-test('updates phase description based on phase number', () => {
+test('static test for phase 1', () => {
   const { getByText } = render(<Phase number="1" />);
+  const phaseDescription = getByText(/Phase 1 - Hero Phase/i);
+  expect(phaseDescription).toBeInTheDocument();
+});
+
+test('static test for phase 2', () => {
+  const { getByText } = render(<Phase number="2" />);
+  const phaseDescription = getByText(/Phase 2 - Movement/i);
+  expect(phaseDescription).toBeInTheDocument();
+});
+
+test('static test for phase 100', () => {
+  const { getByText } = render(<Phase number="100" />);
   const phaseDescription = getByText(/Phase 1 - Hero Phase/i);
   expect(phaseDescription).toBeInTheDocument();
 });
