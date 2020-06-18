62aefdd61f38eb155cf8207707ed18b5de2465d5
Alec Sands <sandsalecv@gmail.com>
2020-06-18 17:20:18 -0500

    added logic to reset phase if phase exceeds possible values

--- warhammer/src/Phase.js
+++ warhammer/src/Phase.js
@@ -2,8 +2,18 @@ import React from 'react';
 import PhaseNames from './data/Phases';
 
 class Phase extends React.Component {
+  constructor(props) {
+    super(props);
+
+    if (this.props.number > PhaseNames.length) {
+      this.state = {number: 1};
+    } else {
+      this.state = {number: this.props.number};
+    }
+  }
+
   render() {
-    return <h3>Phase {this.props.number} - {PhaseNames[this.props.number-1]}</h3>;
+    return <h3>Phase {this.state.number} - {PhaseNames[this.state.number-1]}</h3>;
   }
 }
 
